df9f736cb5dfd448f80cd924731a2257ab0f35aa
Jake <fundsan@gmail.com>
2021-09-09 12:37:38 -0500

    added request.py

---
+++ request.py
@@ -0,0 +1,17 @@
+import response
+data={'age': 39,
+    'workclass': 'State-gov',
+    'fnlgt' : 77516,
+    'education' : 'Bachelors',
+    'education_num' :13,
+    'marital_status': 'Never-married',
+    'occupation' : 'Adm-clerical',
+    'relationship':'Not-in-family',
+    'race':'White',
+    'sex':'Male',
+    'capital_gain':2147,
+    'capital_loss':0,
+    'hour_per_week':40,
+    'native_country':'United-States'}
+r = requests.post('https://fast-river-63740.herokuapp.com//infer/',  json=data)
+print r.json()
